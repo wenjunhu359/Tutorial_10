e2526d4e90e2a615cbdc51b3f1982b29f4196bcd
wenjunhu359 <wh359@drexel.edu>
2015-05-17 20:51:38 -0400

    bash

---
+++ humann.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -cwd
+#$ -V
+#$ -M wh359@drexel.edu
+#$ -P nsftuesPrj
+#$ -l h_rt=4:00:00
+#$ -l h_vmem=8G
+#$ -q all.q@@amdhosts
+#$ -pe shm 2-16
+
+ /etc/profile.d/modules.sh
+module load shared
+module load proteus
+module load sge/univa
+module load gcc/4.8.1
+module load qiime
+
+path=/home/wh359/Tutorial_10/humann-0.99
+cd ${path}
+scons
